0244d0e7635efa6cd9f4eef1c1c501bdecf56165
Charleston Victorino <cv9828@hawaii.edu>
2015-09-25 23:19:58 -1000

    unfixed errors

--- throw_err.py
+++ throw_err.py
@@ -11,7 +11,8 @@ def print_usage():
     sys.stderr.write("available errors: \n")
     sys.stderr.write("\tassertion, io, import, index\n")
     sys.stderr.write("\tkey, name, os, type, value,\n")
-    sys.stderr.write("\tzerodivision\n")
+    sys.stderr.write("\tzerodivision, syntax, overflow, \n")
+    sys.stderr.write("\tattribute, indentation   \n ")
     sys.exit()
 
 # Check args
@@ -21,25 +22,53 @@ if len(sys.argv) != 2:
 error_type = sys.argv[1]
 
 if error_type == "assertion":
-    raise AssertionError
+    assert 1==2
 elif error_type == "io":
-    raise IOError
+    stuff = open('launchcodes.txt')
+    print(stuff)
 elif error_type == "import":
-    raise ImportError
+    from folder import skynet
 elif error_type == "index":
-    raise IndexError
+    aaa = [10, 20]
+    
+    aaa = [1]
+    aaa = [3]
 elif error_type == "key":
-    raise KeyError
+    d = {} 
+    
+    d['key1']
 elif error_type == "name":
-    raise NameError
+    print(h4xx)
 elif error_type == "os":
-    raise OSError
+    import os
+    os.ttyname(3)
 elif error_type == "type":
-    raise TypeError
+    a1=7
+    a2="10"
+    a1+a2
 elif error_type == "value":
-    raise ValueError
+    stuffv = [2,5,9]
+    a, b = stuffv
 elif error_type == "zerodivision":
-    raise ZeroDivisionError
+    print(9000/0)
+elif error_type == "syntax":
+    None 
+    type(None)
+    <type 'NoneType'>
+    None=1
+elif error_type == "overflow":
+    import math 
+    1-math.exp(-1*9999999999*-0.3983230239232)
+elif error_type == "attribute":
+    l = [1,2]
+    1.append(3)
+    l = None
+    l.append(4)
+elif error_type == "indentatation":
+a=1
+if a==1;
+print a
+
 else:
     sys.stderr.write("Sorry, not able to throw a(n) ")
     sys.stderr.write(error_type + " error\n")
